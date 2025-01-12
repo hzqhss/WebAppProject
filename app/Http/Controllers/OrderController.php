9a46c89fcde3bcd6c65c067937d542e6561a9482
[ghzry11] <[ghazi.khairi@gmail.com]>
2025-01-12 14:57:59 +0800

    test

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -1,5 +1,5 @@
 public function show($id)
 {
-    $order = Order::with('items')->findOrFail($id); // Fetch order and related items
+    $order = Order::with('items')->findOrFail($id);
     return view('order_details', compact('order'));
 }
